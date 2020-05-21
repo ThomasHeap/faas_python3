2ff4639dbcb69706a7a339f6a90f6fa93c9dcfdb
Thomas <thomaseheap@gmail.com>
2020-05-21 12:41:48 +0100

    run infernece changes

--- faas_helper_fns.py
+++ faas_helper_fns.py
@@ -34,7 +34,7 @@ def sim(th, eps=[0]*94):
 def simulator(th):
 
     with get_context("forkserver").Pool() as pool:
-        print(th)
+        print(th.numpy())
         sims = pool.map(sim, th.numpy())
         pool.close()
         pool.join()
